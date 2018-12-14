0aa9b7cc8b581d87d2ae21325ce23ceda83a7d3d
Linus Behrbohm <linusbehrbohm@web.de>
2018-12-14 01:04:25 +0100

    Show help when unknown command is entered

--- src/Input.hs
+++ src/Input.hs
@@ -17,26 +17,22 @@ type Input = String
 quitCommands :: [String]
 quitCommands = ["q", ":Q", ":q","quit","exit"]
 
-helpCommands :: [String]
-helpCommands = [":h","help","?"]
-
 getInput :: IO (Maybe Input)
 getInput = getLine >>= return . Just --(stdin `ifReadyDo` getLine)
 
 processInput :: IO Game -> Maybe Input -> IO Game
 processInput game Nothing = game
 processInput game (Just input)
-                      | input `elem` helpCommands = printHelp >> game
                       | input `elem` quitCommands = game >>= return . quitGame
-                      | otherwise = game
+                      | otherwise = printHelp >> game
 
 setEcho :: Bool -> IO()
 setEcho b = hSetEcho stdin b
 
-getHelp :: IO()
-getHelp = foldr (++) " to quit.\n" $("Type ":list)
+printHelp :: IO()
+printHelp = putStrLn .foldr (++) " to quit.\n" $("Type ":list)
   where
-    list = foldr (:) (" or ":[last quitCommands]) $merged
+    list = foldr (:) ("or ":[last quitCommands]) $merged
     merged = concat . transpose $[take tosep quitCommands, seperators]
     tosep = length quitCommands-2
     seperators = take tosep.cycle $[", "]
