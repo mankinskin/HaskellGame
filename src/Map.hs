1c5575055c2e6d86f248b6a5bc597a3ccc91a363
Linus Behrbohm <linusbehrbohm@web.de>
2018-12-14 16:47:42 +0100

    Make Maps use Array and provide generic interface
    makeMap creates a map with given initial values
    markPos/-Path sets the value(s) for position(s) in a Map

--- src/Map.hs
+++ src/Map.hs
@@ -1,13 +1,34 @@
 module Map where
 
-import System.IO
+import Data.Array
 
+type MapVec2 = (Int, Int) -- Vec in a Map
+type Path = [MapVec2] -- List of Vecs
 
-data Location = Impassable | Room deriving Show
+-- 2D Map
+data Map a = Map (Array MapVec2 a)
 
-data Map = Map [[Location]]
-
-instance Show Map where
-  show (Map locations) = foldr (++) "" (map foldLine locations)
+instance (Show a) => Show (Map a) where
+  show (Map arr) = foldr (++) "" (lines sizey)
     where
-      foldLine line = foldr (++) "\n" ((show.head $line):(map (((++) "\t") . show).init $line))
+      (_, (sizex, sizey)) = bounds arr
+      lines 0 = []
+      lines y = (foldr (++) "\n" (line (sizey-y) sizex)):(lines (y-1))
+      line _ 0 = []
+      line y x = show (arr!((sizex-x), y)):(line y (x-1))
+
+
+makeMap :: MapVec2 -> a -> (Map a)
+-- Creates a Map of a given size and with given initial value
+makeMap (sx, sy) zro = Map (listArray ((0, 0), (sx, sy)) arr)
+ where arr = take ((sx+1)*(sy+1)).cycle $[zro]
+
+
+markPath :: (Map a) -> Path -> [a] -> (Map a)
+-- sets values of given positions in Map
+markPath (Map arr) ps vals = Map (arr//updates)
+  where updates = (zip ps vals) --updated assocs
+
+markPos :: (Map a) -> MapVec2 -> a -> (Map a)
+-- sets values of given positions in Map
+markPos map p val = markPath map [p] [val]
