6b33e35b4c6f43c6dd5b2d458b9392e38b9482e2
Linus Behrbohm <linusbehrbohm@web.de>
2018-12-16 01:17:05 +0100

    fix initializing Map with too much data (off-by-one)

--- src/Map.hs
+++ src/Map.hs
@@ -6,22 +6,22 @@ type MapVec2 = (Int, Int) -- Vec in a Map
 type Path = [MapVec2] -- List of Vecs
 
 -- 2D Map
-data Map a = Map (Array MapVec2 a)
+data Map a = Map {arr::Array MapVec2 a}
 
 instance (Show a) => Show (Map a) where
-  show (Map arr) = foldr (++) "" (lines sy)
+  show (Map arr) = lines sy
     where
       (sx, sy) = snd.bounds $arr
-      lines 0 = []
-      lines yi = (foldr (++) "\n" (line (sy-yi) sx)):lines (yi-1)
-      line _ 0 = []
-      line yi xi = show (arr!(sx-xi, yi)):(line yi (xi-1))
+      lines 0 = line sy sx
+      lines yi = line (sy-yi) sx++lines (yi-1)
+      line yi 0 = show (arr!(sx-0, yi)) ++ "\n"
+      line yi xi = show (arr!(sx-xi, yi)) ++ (line yi (xi-1))
 
 
 makeMap :: MapVec2 -> a -> (Map a)
 -- Creates a Map of a given size and with given initial value
-makeMap (sx, sy) zro = Map (listArray ((0, 0), (sx, sy)) arr)
- where arr = take ((sx+1)*(sy+1)).cycle $[zro]
+makeMap (sx, sy) zro = Map (listArray ((0, 0), (sx-1, sy-1)) arr)
+ where arr = take ((sx)*(sy)).cycle $[zro]
 
 
 markPath :: (Map a) -> Path -> [a] -> (Map a)
