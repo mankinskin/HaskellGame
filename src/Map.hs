71a559566b7773f841f14f7f5732a7903c1d4b67
Linus Behrbohm <linusbehrbohm@web.de>
2018-12-16 02:28:48 +0100

    Print borders when showing Map

--- src/Map.hs
+++ src/Map.hs
@@ -17,7 +17,8 @@ instance Show Map where
         (_, (sx, sy)) = bounds visits
         (_, (wsx, wsy)) = bounds walls
 
-        lines = mapline sx sy
+        lines = topborder ++"\n"++show Wall ++ mapline sx sy
+          where topborder = concat.take ((sx+1)*2+1) $(cycle [show $Wall])
 
         -- double recursion, baby
         -- append shown Walls and Visit values of Map
@@ -43,17 +44,17 @@ instance Show Map where
             where
                   tile = show (visits!visit xi yi)
                   wall = show (walls!hwall xi yi)
-                  next 0 = "\n"++wallline sx yi
+                  next 0 = "\n"++show Wall ++wallline sx yi
                   next nxi = mapline (nxi-1) yi
 
-        wallline _ 0 =
-          "\n"
         wallline xi yi =
-          wall ++ show Wall ++ next xi
+          wall ++ show Wall ++ next xi yi
             where
                   wall = show (walls!vwall xi yi)
-                  next 0 = "\n"++mapline sx (yi-1)
-                  next nxi = wallline (nxi-1) yi
+                  next 0 0 = "\n"
+                  next nxi 0 = wallline (nxi-1) 0
+                  next 0 nyi = "\n"++show Wall++mapline sx (nyi-1)
+                  next nxi nyi= wallline (nxi-1) nyi
 
 
 makeMap :: IntVec2 -> Map
